5e5c09df0ee6af0df591b2cdb5ff76d76d95e562
unknown <gaurav.paradkar91@gmail.com>
2024-04-09 18:06:57 +0530

    First commit

---
+++ backend/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+    //mongoUrl: "mongodb+srv://FlyingPanda730:UgpngUX6DVAx1Xq0@cluster0.ormku05.mongodb.net/?retryWrites=true&w=majority",
+    mongoUrl: "mongodb://localhost:27017/instagram",
+    Jwt_secret: "faslkfocvneofu"
+
+};
